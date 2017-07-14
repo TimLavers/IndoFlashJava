b8ae5e5cbd3c6685d51fc1114b1f22c5f0669822
TimLavers <tim@grandtestauto.org>
2017-07-14 16:45:48 +1000

    Added some more tests.

--- app/src/test/java/org/grandtestauto/indoflash/SpecTest.java
+++ app/src/test/java/org/grandtestauto/indoflash/SpecTest.java
@@ -2,6 +2,10 @@ package org.grandtestauto.indoflash;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import static org.grandtestauto.indoflash.WordListSpecTest.parseNode;
 
 public class SpecTest {
 
@@ -12,5 +16,13 @@ public class SpecTest {
     @Test
     public void testTitle() {
         Assert.assertEquals("The title", spec("The title").title());
+    }
+
+    @Test
+    public void constructFromNode() throws Exception {
+        String xml = "<WordList><Title>Lesson 1</Title><File>lesson1</File></WordList>";
+        Node appNode = parseNode(xml, WordListSpec.TAG);
+        Spec list = new Spec((Element) appNode);
+        Assert.assertEquals("Lesson 1", list.title());
     }
 }
--- app/src/test/java/org/grandtestauto/indoflash/WordListSpecTest.java
+++ app/src/test/java/org/grandtestauto/indoflash/WordListSpecTest.java
@@ -2,11 +2,27 @@ package org.grandtestauto.indoflash;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class WordListSpecTest {
 
     private static WordListSpec wordListSpec(String title, String fileName) {
         return new WordListSpec(title, fileName);
+    }
+
+    static Node parseNode(String xml, String tag) throws Exception {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputStream stream = new ByteArrayInputStream(xml.getBytes("utf-8"));
+        Document document = builder.parse(stream);
+        return document.getElementsByTagName(tag).item(0);
     }
 
     @Test
@@ -20,5 +36,20 @@ public class WordListSpecTest {
         String title = "The title";
         String fileName = "The file";
         Assert.assertEquals(fileName, wordListSpec(title, fileName).fileName());
+    }
+
+    @Test
+    public void toStringTest() {
+        String title = "The title";
+        Assert.assertEquals(title, wordListSpec(title, "The file").toString());
+    }
+
+    @Test
+    public void constructFromNode() throws Exception {
+        String xml = "<WordList><Title>Lesson 1</Title><File>lesson1</File></WordList>";
+        Node appNode = parseNode(xml, WordListSpec.TAG);
+        WordListSpec list = new WordListSpec((Element) appNode);
+        Assert.assertEquals("Lesson 1", list.title());
+        Assert.assertEquals("lesson1", list.fileName());
     }
 }
